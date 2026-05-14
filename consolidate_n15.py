1e29bf7a47f93ce9cc4f84478af8068f7f47af09
Robin Leroy <egg.robin.leroy@gmail.com>
2026-05-14 19:09:26 +0200

    A failed attempt at rescuing the lists

--- consolidate_n15.py
+++ consolidate_n15.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 import re
+from typing import Literal
 
 TRACKED_TAGS = ("body", "span", "p", "ol", "ul", "li", "h1", "h2", "h3", "h4", "a")
 
@@ -43,7 +44,11 @@ class N15Consolidator(HTMLParser):
             continue
           if output_tag(candidate) == "span":
             continue
-          self.out += f"<{output_tag(candidate)}\n>"
+          attributes = ""
+          classes = candidate[1]
+          if "start" in classes:
+            attributes = " class=start"
+          self.out += f"<{output_tag(candidate)}{attributes}\n>"
           self.committed_stack.append((i, candidate))
         self.out += data
 
@@ -51,11 +56,43 @@ def output_tag(stack_element : tuple[str, list[str]]):
   tag, classes = stack_element
   if tag == "span" and "c13" in classes:
     return "i"
+  if tag == "p" and "c26" in classes:
+    # <p> that should be in <li>, turned back to <p> by the list fixup below.
+    return "pl"
   return tag
 
 consolidator = N15Consolidator()
 with open("DRAFT10646MAbicameralToR.docx.html", encoding="utf8") as f:
   consolidator.feed(f.read())
+
+list_stack : list[Literal['o','u']] = []
+
+def fixup_end_of_list(m: re.Match[str]):
+  global list_stack
+  if m.group("opening"):
+    list_stack.append(m.group("opening")[1])
+    return m.group(1)
+  else:
+    if "</pl\n>" in m.group("closing"):
+      return "</p></li\n>"
+    if m.group("opening_ahead"):
+      if m.group("opening_ahead") == "<pl\n>":
+        return "\n<p\n>"
+      elif m.group("start"):
+        list_stack.append(m.group("opening_ahead")[1])
+        return m.group("opening_ahead")
+      else:
+        if not list_stack:
+          return f"\n<!--ERROR empty list stack at {m.group(0)}-->"
+        return f"</li></{list_stack.pop()}l></li\n>"
+    else:
+      result = ""
+      while list_stack:
+        result += f"</li></{list_stack.pop()}l>"
+      return result
+
+
 with open("consolidated-N15.html", "w", encoding="utf8") as out:
+  fixed_lists = re.sub(r"(?P<opening><[ou]l class=start\n>)|(?P<closing>(?:</li\n></[ou]l\n>|</pl\n>)(?P<opening_ahead><[pou]l(?P<start> class=start)?\n>)?)", fixup_end_of_list, consolidator.out)
   out.write("<meta charset=utf-8>\n")
-  out.write(consolidator.out)
+  out.write(fixed_lists)
